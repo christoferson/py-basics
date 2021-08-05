f204379612afe733036e2d3ea5b8e7c8f6a8224e
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-05 21:36:16 +0900

    Add demo_datatypes

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+
+def demo_datatypes():
+    x = "String Value"
+    x = 51 # Int
+    x = 82.5 # Float
+    x = 3j # Complex 
+    x = ["diamond", "spade", "flower"] # List
+    x = ("diamond", "spade", "flower") # Tuple
+    x = range(7) # Range
+    x = {"name" : "Conan", "age" : 24} # Dictionary
+    x = {"diamond", "spade", "flower"} # Set
+    x = frozenset({"diamond", "spade", "flower"}) # Frozen Set
+    x = True # Boolean
+    x = b"Hello" # Bytes
+    x = bytearray(5) # Byte Array
+    x = memoryview(bytes(5)) # Memory View
