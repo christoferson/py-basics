2a0c2ac18c31f8d2e84f08d2c46c0107950e5674
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-05 14:49:56 +0900

    Add Character.AttackMonster

--- character.py
+++ character.py
@@ -1,6 +1,11 @@
+import monster
+
 class Character:
   def __init__(self, name):
     self.name = name
 
   def attack(self):
     print(self.name + " attacking...")
+
+  def attackMonster(self, m : monster.Monster):
+    print("%s attacking %s ..." %(self.name, m.name))
--- monster.py
+++ monster.py
@@ -1,6 +1,7 @@
 class Monster:
-  def __init__(self, name):
+  def __init__(self, name, life):
     self.name = name
+    self.life = life
 
   #def attack(self):
   #  print(self.name + " attacking...")
