62bcae93990f4cea4d149b3370e8fe2052d52c9a
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-05 22:12:59 +0900

    tuples update

--- demo/tuples.py
+++ demo/tuples.py
@@ -3,4 +3,15 @@ def demo_tuples():
     print("*************** Demo Tuples ***************")
     mytuple = ("apple", "banana", "cherry", "apple", "cherry")
     print(mytuple)
+    # Access
     print(mytuple[1:3])
+    print(mytuple[1:])
+    print(mytuple[:3])
+    # Unpack
+    (e1, e2, *ex) = mytuple
+    print(e1)
+    print(e2)
+    print(ex)
+    # For Loop
+    for mytuple_element in mytuple:
+        print(mytuple_element)
