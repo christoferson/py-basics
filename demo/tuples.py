86f012abec1f17c985b6b2f5d7deeaf8cd48a7a5
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-05 21:57:02 +0900

    Add demo_tuples

---
+++ demo/tuples.py
@@ -0,0 +1,6 @@
+
+def demo_tuples():
+    print("*************** Demo Tuples ***************")
+    mytuple = ("apple", "banana", "cherry", "apple", "cherry")
+    print(mytuple)
+    print(mytuple[1:3])
