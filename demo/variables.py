d779c61e8e9a85582ea3a39e2574971deb2168ba
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-10-04 18:31:07 +0900

    Add demo_declaration

--- demo/variables.py
+++ demo/variables.py
@@ -1,9 +1,22 @@
+# Demo Variables
+
+
 def demo_variables():
     print("*************** Demo Variables ***************")
 
-    # Python has no command for declaring a variable.
+    demo_declaration()
+
+def demo_declaration():
+    
+    # 1.Python has no command for declaring a variable.
 
     x = 5
     y = "stringvar"
 
     print(x, y)
+
+    # 2. Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+    x = 4       # x is of type int
+    x = "Sally" # x is now of type str
+    print(x)
