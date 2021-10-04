8ce88865010601df1e21341d6b1f88ed8bd1c77c
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-10-04 13:39:13 +0900

    Add demo_variables

---
+++ demo/variables.py
@@ -0,0 +1,9 @@
+def demo_variables():
+    print("*************** Demo Variables ***************")
+
+    # Python has no command for declaring a variable.
+
+    x = 5
+    y = "stringvar"
+
+    print(x, y)
