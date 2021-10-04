3c11c2f07aca20939d33af331f39bea75c74c5ac
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-10-05 01:17:28 +0900

    Add demo_type

--- demo/variables.py
+++ demo/variables.py
@@ -6,6 +6,10 @@ def demo_variables():
 
     demo_declaration()
 
+    demo_casting()
+
+    demo_type()
+
 def demo_declaration():
     
     # 1.Python has no command for declaring a variable.
@@ -20,3 +24,19 @@ def demo_declaration():
     x = 4       # x is of type int
     x = "Sally" # x is now of type str
     print(x)
+
+def demo_casting():
+
+    x = str(3)    # x will be '3'
+    y = int(3)    # y will be 3
+    z = float(3)  # z will be 3.0 
+
+    print(x, y, z, sep="|")
+
+
+def demo_type():
+
+    x = 5
+    y = "John"
+    print(type(x))
+    print(type(y)) 
