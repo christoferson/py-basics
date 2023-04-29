f7c09a6b0f09b595fbff7fd227f3b887042d9ef9
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-04-29 19:05:32 +0900

    Add DataTypes

---
+++ lesson/datatypes.py
@@ -0,0 +1,27 @@
+x = 3
+print(3)
+
+lists = [10, "foo", 200.5]
+
+dictionary = {
+    "foo": "bar",
+    "boo": "zoo"
+}
+
+y = 0.1+0.2-0.3
+print(y)
+
+# Variable Assignment
+cats = 3
+
+# Dynamic typing
+dogs = 2
+print(dogs)
+
+dogs = ["Sammy", "Franky"]
+print(dogs)
+
+# Variable Type
+
+print(type(cats))
+print(type(dogs))
