0d6fd21a5f9812720673e8c897f80528e49ca017
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-05-07 02:13:45 +0900

    Add booleanvar

---
+++ lesson/booleanvar.py
@@ -0,0 +1,8 @@
+# Boolean
+
+# Boolean literal is case sensitive
+myboolean = True # true is not equivalent to True
+print(type(myboolean), myboolean)
+
+myboolean = False # false is not equivalent to False
+print(type(myboolean), myboolean)
