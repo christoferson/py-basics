faf076ea56af34c37dbfb4345d4716cb37b73e32
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-05-07 21:36:04 +0900

    Update Base IO

--- lesson/basicio.py
+++ lesson/basicio.py
@@ -1,10 +1,15 @@
 # Basic File IO
 
 import os
+import locale
+import time
 
 # Check current working directory
 cwd = os.getcwd()
 print(cwd)
+
+# Check Locale
+print(locale.getpreferredencoding())
 
 # Straightforward way of opening then closing file
 #myfile = open("lesson/basicio.txt")
@@ -12,8 +17,15 @@ print(cwd)
 #print(lines)
 #myfile.close()
 
-with open("lesson/basicio.txt") as myfile:
+# Open file for reading
+with open("lesson/basicio.txt", mode="r", encoding="UTF-8") as myfile:
   lines = myfile.readlines()
   print(lines)
+  # myfile.write("xxx") # io.UnsupportedOperation: not writable
   # autoclose file
 print(lines)
+
+# Open file for writing (append mode)
+with open("lesson/basicio.txt", mode="a", encoding="UTF-8") as myfile:
+  #myfile.write("\n")
+  myfile.write(str(time.gmtime(0)) + "\n")
