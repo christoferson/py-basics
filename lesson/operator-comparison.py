1f3382b541c93678f165457fb52bad6cb4252084
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-05-08 00:04:40 +0900

    Add Operator Coparison

---
+++ lesson/operator-comparison.py
@@ -0,0 +1,24 @@
+
+# Equality ==
+print(2 == 2)
+print(2 == 1)
+print('2' == 2)
+print('Foo' == 'foo')
+
+# NonEquality !=
+print(2 != 2)
+print(2 != 1)
+print('2' != 2)
+print('Foo' != 'foo')
+
+# Greater Than > 
+print(3 > 2)
+
+# Greater Than or Equal To >= 
+print(3 >= 3)
+
+# Less Than < 
+print(3 < 2)
+
+# Less Than or Equal To <= 
+print(3 <= 3)
