c59df2966dfaabd383b197a6d66ee9a4ea35c89d
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-05-08 22:40:37 +0900

    Control Flow

---
+++ lesson/controlflow.py
@@ -0,0 +1,25 @@
+# Control Flow
+
+# If / Else If / Else
+
+myvar = 1
+
+if myvar > 2:
+    print("IF")
+elif myvar > 3:
+    print("ELIF")
+else:
+    print("ELSE")
+
+# 
+
+storetype = "BAKERY"
+
+if storetype == "CAFE":
+    print("Coffee")
+elif storetype == "BANK":
+    print("Check")
+elif storetype == "BAKERY":
+    print("Donuts")
+else:
+    print("???")
