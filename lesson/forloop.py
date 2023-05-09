8432fe4a3326fe3cf7fa44cb5d55314b11b73c9e
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-05-09 23:56:17 +0900

    Add forloop.py

---
+++ lesson/forloop.py
@@ -0,0 +1,23 @@
+# For loop
+
+
+#  Sample 1
+
+myitems = [1,2,3,4,5]
+
+for item in myitems:
+    print(item)
+
+# Sample 2
+
+mytuples = [(1,2),(3,4),(5,6)]
+
+for a,b in mytuples:
+    print(f"A={a} B={b}")
+
+# Sample 3
+
+mydictionary = {"K1": 3, "K2": 2}
+
+for k,v in mydictionary.items():
+    print(f"{k}={b}")
