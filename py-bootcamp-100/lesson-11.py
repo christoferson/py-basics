bb3be2d9328ede2e7987f52db3f92c919a018ade
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-07-18 01:05:57 +0900

    Lesson 9,11

---
+++ py-bootcamp-100/lesson-11.py
@@ -0,0 +1,5 @@
+# Input
+print("Hi " + input("Enter Name: "))
+
+# Challenge
+print("Hi " + str(len(input("Enter Name: "))))
---
+++ py-bootcamp-100/lesson-9.py
@@ -0,0 +1,2 @@
+# String Concatenation and New Line
+print("---\n1. Hello World\n2. Hellow World\n" + "3. Foo\n---\n");
