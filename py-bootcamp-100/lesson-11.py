4165c711df0e23ff1327de70ce9da8b006a3c68e
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-07-18 22:42:51 +0900

    lesson 11

--- py-bootcamp-100/lesson-11.py
+++ py-bootcamp-100/lesson-11.py
@@ -2,4 +2,25 @@
 print("Hi " + input("Enter Name: "))
 
 # Challenge
-print("Hi " + str(len(input("Enter Name: "))))
+print("Hi-L1 " + str(len(input("Enter Name: "))))
+
+# Variable
+name = input("Enter Name: ")
+print("Hi-L2 " + str(len(name)))
+
+# Challenge
+a = input("A: ")
+b = input("B: ")
+
+tmp = a
+a = b
+b = tmp
+
+print("A: " + a + " B: " + b) 
+
+# Final Challenge
+
+print("Welcome to the Band Name generator!")
+city = input("What is the name of the city you grew up in? \n")
+pet = input("What is the name of your pet? \n")
+print(city + " " + pet)
