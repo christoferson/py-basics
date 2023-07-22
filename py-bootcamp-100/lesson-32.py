9647ae3dccf2d674cc580785f6e1e9d7e1fbf429
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-07-22 22:54:08 +0900

    lesson 32

--- py-bootcamp-100/lesson-32.py
+++ py-bootcamp-100/lesson-32.py
@@ -56,4 +56,60 @@ if want_cheeze == "Y":
 
 print(f"Pizza cost: ${cost}")
 
-   
+# Leap Year - V2
+year = 2400
+
+if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    print("leap year")
+elif year % 4 == 0 and year % 100 != 0:
+    print("leap year")
+else:
+    print("not div by 4, not leap year")
+
+# Love Calculator
+
+him = input("His Name: ")
+her = input("Her Name: ")
+
+# Version 1
+
+him = him.lower()
+her = her.lower()
+
+count_true = 0
+count_true += him.count("t") + him.count("r") + him.count("u") + him.count("e")
+count_true += her.count("t") + her.count("r") + her.count("u") + her.count("e")
+
+count_love = 0
+count_love += him.count("l") + him.count("o") + him.count("v") + him.count("e")
+count_love += her.count("l") + her.count("o") + her.count("v") + her.count("e")
+
+print(f"{count_true} {count_love} {str(count_true) + str(count_love)}")
+
+# Version 2
+
+pair = him.lower() + her.lower()
+
+t = pair.count("t")
+r = pair.count("r")
+u = pair.count("u")
+e = pair.count("e")
+sum_true = t + r + u + e 
+
+l = pair.count("l")
+o = pair.count("o")
+v = pair.count("v")
+e = pair.count("e")
+sum_love = l + o + v + e
+
+print(f"t={t} r={r} u={u} e={e} {sum_true}")
+print(f"l={l} o={o} v={v} e={e} {sum_love}")
+print(f"{sum_true}{sum_love}")
+
+love_score = int(str(sum_true) + str(sum_love))
+if (love_score > 10 and love_score < 90):
+    print(f"Your score is {love_score}, you go together like coke and methos")
+elif (love_score > 40 and love_score < 50):
+    print(f"Your score is {love_score}, you are alright together")
+else:
+    print(f"Your score is {love_score}")
