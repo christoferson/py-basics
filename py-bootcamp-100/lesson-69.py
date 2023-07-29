59030020d664a235eb76c13ceac0be6f7f1c27bd
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-07-29 20:55:46 +0900

    lesson 69

--- py-bootcamp-100/lesson-69.py
+++ py-bootcamp-100/lesson-69.py
@@ -4,11 +4,19 @@ word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
+wrk_list = []
+for idx in range(0, len(chosen_word)):
+   wrk_list.append("_")
+
 guess = input("Enter a letter: ").lower()[0]
 
-if chosen_word.find(guess) > -1:
-    print("Found")
+guess_idx = chosen_word.find(guess)
+if guess_idx > -1:
+    wrk_list[guess_idx] = guess
+    print(f"Found {guess} at {guess_idx}")
 else:
     print("Not Found")
 
-print(chosen_word, " ", guess)
+print(chosen_word, " ", guess, "\n")
+
+print(wrk_list)
