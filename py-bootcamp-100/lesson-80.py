600be9f51b67b5b485a85f6a0e473eabb34b5714
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-08-10 01:07:05 +0900

    lesson 80 ceasar

--- py-bootcamp-100/lesson-80.py
+++ py-bootcamp-100/lesson-80.py
@@ -12,23 +12,31 @@ def decode(cipher_text : str, shift_amount : int):
         plaint_text += letter_decoded
     return plaint_text
 
+def ceasar(plain_text : str, shift_amount : int):
+    cipher_text = ""
+    for letter in plain_text:
+        letter_encoded = chr(ord(letter) + shift_amount)
+        cipher_text += letter_encoded
+    return cipher_text
+
 def main():
 
     cmd = input("Type encode to encode, decode to decode: ")
 
     cmd_elements = cmd.split(" ")
-    if len(cmd_elements) != 2:
+    if len(cmd_elements) != 3:
         print("Command Syntax invalid. encode|decode text.")
         return
 
     op = cmd.split(" ")[0].lower()
     text = cmd.split(" ")[1]
+    shift = int(cmd.split(" ")[2])
 
     if "encode" == op:
-        cipher_text = encode(text, 2)
+        cipher_text = ceasar(text, shift)
         print(f"Cipher: {cipher_text}") 
     else:
-        plain_text = decode(text, 2)
+        plain_text = ceasar(text, -shift)
         print(f"Plain:  {plain_text}") 
 
 
