32afde2109dc4de9c748c06765cbce547e745578
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-08-13 22:16:52 +0900

    lesson 80

--- py-bootcamp-100/lesson-80.py
+++ py-bootcamp-100/lesson-80.py
@@ -19,7 +19,17 @@ def ceasar(plain_text : str, shift_amount : int):
         cipher_text += letter_encoded
     return cipher_text
 
-def main():
+def ceasar_v2(plain_text : str, shift_amount : int):
+    cipher_text = ""
+    for letter in plain_text:
+        if " " == letter or letter.isnumeric():
+            cipher_text += letter
+            continue
+        letter_encoded = chr(ord(letter) + shift_amount)
+        cipher_text += letter_encoded
+    return cipher_text
+
+def process():
 
     cmd = input("Type encode to encode, decode to decode: ")
 
@@ -30,14 +40,24 @@ def main():
 
     op = cmd.split(" ")[0].lower()
     text = cmd.split(" ")[1]
-    shift = int(cmd.split(" ")[2])
+    shift = int(cmd.split(" ")[2]) % 23
 
     if "encode" == op:
-        cipher_text = ceasar(text, shift)
+        cipher_text = ceasar_v2(text, shift)
         print(f"Cipher: {cipher_text}") 
     else:
-        plain_text = ceasar(text, -shift)
+        plain_text = ceasar_v2(text, -shift)
         print(f"Plain:  {plain_text}") 
 
+def process_main():
+    continue_flag = True
+    while continue_flag:
+        process()
+        continue_reply = input("Type yes to repeat: ")
+        if continue_reply.lower() != "yes":
+            continue_flag = False
+            print("Goodbye")
 
-main()
+if __name__ == "__main__":
+    print("A")
+    process_main()
