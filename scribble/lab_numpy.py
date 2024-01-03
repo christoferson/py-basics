d80acf5c9f10b1bb130584fd6fb9a0030fa1eeba
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-01-04 03:14:45 +0900

    numpy

--- scribble/lab_numpy.py
+++ scribble/lab_numpy.py
@@ -178,3 +178,20 @@ print(newarr)
 arr = np.array([1, 2, 3, 4, 5])
 for x in arr:
   print(x) 
+
+# Iterating 2-D Arrays
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+for x in arr:
+  print(x) 
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+for x in arr:
+  for y in x:
+    print(y) 
+
+# Iterating 3-D Arrays
+    
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+for x in arr:
+  print(x) 
