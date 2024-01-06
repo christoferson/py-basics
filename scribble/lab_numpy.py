3fc144eeacf3a5b52e3014643ef57b672dd84198
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-01-06 19:55:32 +0900

    numpy

--- scribble/lab_numpy.py
+++ scribble/lab_numpy.py
@@ -192,6 +192,6 @@ for x in arr:
 
 # Iterating 3-D Arrays
     
-arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [["a", "b", "c"], [10, 11, 12]]])
 for x in arr:
   print(x) 
